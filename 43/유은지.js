44b8cb52fdc286c0ec9fe07f7f6de7f055a5779c
유은지 <aine0227@naver.com>
2022-06-15 10:20:37 +0900

    Update 유은지.js

--- 43/유은지.js
+++ 43/유은지.js
@@ -1,1 +1,15 @@
-일단 폴더만들기
+var isSubPath = function(head, root) {
+
+    const dfs = (head, node) => {
+    if(!head) return true
+    if(!node) return false
+    if(node.val !== head.val) return false
+    return dfs(head.next, node.left) || dfs(head.next, node.right)
+    } 
+    
+    if(!root) return false
+    if(dfs(head, root)) return true
+
+    return isSubPath(head, root.left) || isSubPath(head, root.right)
+    
+};
