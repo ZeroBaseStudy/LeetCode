c26ebc85bb28b84a2620cbfbb79d48313329b3a5
유은지 <aine0227@naver.com>
2022-07-06 13:25:18 +0900

    Create 유은지.js

---
+++ 49. Group Anagrams/유은지.js
@@ -0,0 +1,10 @@
+const groupAnagrams = function (strs) {
+    const map = new Map();
+
+    strs.forEach(v => {
+        const key = v.split('').sort().join('');
+        map.has(key) ? map.set(key, [...map.get(key), v]) : map.set(key, [v]);
+    })
+
+    return [...map.values()];
+};
